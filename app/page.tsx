41780af5ea02ad4413733349cadd10c1d7235e96
Yuliya Kasavets <kosoveculia8@gmail.com>
2025-10-31 18:31:29 +0300

    added dashboard(sidebar)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header';
 import { auth } from './auth';
 import LoginView from './LoginView';
+import Sidebar from '@/components/Sidebar';
 
 export default async function Home() {
   const session = await auth();
@@ -9,8 +10,14 @@ export default async function Home() {
   return (
     <div>
       {user && (
-        <div className="p-4">
+        <div className="p-4 h-screen">
           <Header user={user} />
+          <div className="mt-4 grid grid-cols-12 gap-4">
+            <div className="col-span-3">
+              <Sidebar />
+            </div>
+            <div className="col-span-9 bg-white rounded-2xl">content</div>
+          </div>
         </div>
       )}
       {!user && <LoginView />}
---
+++ components/Sidebar.tsx
@@ -0,0 +1,53 @@
+import {
+  AlignJustifyIcon,
+  BellIcon,
+  HeadsetIcon,
+  LaptopIcon,
+  PackagePlusIcon,
+  PlugIcon,
+} from 'lucide-react';
+import Link from 'next/link';
+
+export default function Sidebar() {
+  return (
+    <aside
+      style={{ minHeight: `calc(100vh - 80px)` }}
+      className="bg-white rounded-2xl p-4 h-full"
+    >
+      <h2 className="uppercase text-gray-600 text-xs font-extrabold mb-2">
+        Navigation
+      </h2>
+      <nav className="flex flex-col gap-2 *:flex *:gap-1 *:items-center">
+        <Link href={'/products'}>
+          <AlignJustifyIcon className="h-5" />
+          All products
+        </Link>
+        <Link href={'/electronics'}>
+          <PackagePlusIcon className="h-5" />
+          Add product
+        </Link>
+        <Link href={'/electronics'}>
+          <BellIcon className="h-5" />
+          Notifications
+        </Link>
+      </nav>
+      <h2 className="uppercase text-gray-600 text-xs font-extrabold mt-6 mb-2">
+        Categories
+      </h2>
+      <nav className="flex flex-col gap-2 *:flex *:gap-1 *:items-center">
+        <Link href={'/electronics'}>
+          <PlugIcon className="h-5" />
+          Electronics
+        </Link>
+        <Link href={'/headsets'}>
+          <HeadsetIcon className="h-5" />
+          Headsets
+        </Link>
+        <Link href={'/laptops'}>
+          <LaptopIcon className="h-5" />
+          Laptops
+        </Link>
+      </nav>
+    </aside>
+  );
+}
